142f42dace038e8e1448180ecb2617462b6921c8
Alexander Lee <10814755+alexlee22@users.noreply.github.com>
2019-09-29 17:28:25 +1000

    Updates to auth

--- index.js
+++ index.js
@@ -23,83 +23,6 @@ const SCOPES = [
 const TOKEN_PATH = 'token.json';
 
 
-/*
-function accessGoogle(selectedFunction) {
-  // Load client secrets from a local file.
-  fs.readFile('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    // Authorize a client with credentials, then call the Google Calendar API.
-    authorize(JSON.parse(content), selectedFunction);
-  });
-}
-
-
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-    //return oAuth2Client;
-  });
-}
-
-function getAccessToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
-
-function listEvents(auth) {
-  //console.log(auth)
-  const calendar = google.calendar({version: 'v3', auth});
-  return calendar.events.list({
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    
-    if (events.length) {
-      console.log('Upcoming 10 events:');
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-      });
-    } else {
-      console.log('No upcoming events found.');
-    }
-    
-  });
-}
-*/
-
 // - - - - - - - - - - - - - - - - - - - -
 //  GOOGLE API
 // - - - - - - - - - - - - - - - - - - - -
@@ -121,15 +44,6 @@ function listEvents(auth, timeMin, timeMax) {
   });
 }
 
-
-
-
-
-
-
-
-
-
 function createEvent(auth, year, month, day, hour, minute){
   let postStartDateTime = new Date(Date.UTC(year, month, day, hour, minute, 0))
   let postEndDateTime = new Date(Date.UTC(year, month, day, hour, minute + APPOINTMENT_DURATION_MINUTES + APPOINTMENT_BREAK_MINUTES, 0))
@@ -141,8 +55,6 @@ function createEvent(auth, year, month, day, hour, minute){
       'calendarId': 'primary',
       'resource': {
         'summary': 'Booked Event',
-        //'location': '800 Howard St., San Francisco, CA 94103',
-        //'description': 'A chance to hear more about Google\'s developer products.',
         'start': {
           'dateTime': postStartDateTime.toISOString(),
           'timeZone': 'UTC'
@@ -215,26 +127,49 @@ function getAccessToken(oAuth2Client) {
   })
 }
 
-// - - - - - - - - - - - - - - - - - - - -
-//
-// - - - - - - - - - - - - - - - - - - - -
 
-
+// - - - - - - - - - - - - - - - - - - - -
+// FUNCTIONS
+// - - - - - - - - - - - - - - - - - - - -
 
 const APPOINTMENT_START_HOUR = 9;
 const APPOINTMENT_END_HOUR = 18;
 const APPOINTMENT_DURATION_MINUTES = 40;
 const APPOINTMENT_BREAK_MINUTES = 5;
 
+function checkWeekday(date){
+  if (0 < date.getDay() && date.getDay() < 6){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//
 function getAppointmentTimes(year, month, day){
+  //Check if weekend, return empty list
+  let constDate = new Date(Date.UTC(year, month-1, day, 0, 0, 0));
+  if (!checkWeekday(constDate)){
+    return []; 
+  }
+
   let datetime_list = [];
   for (let i = APPOINTMENT_START_HOUR; i < APPOINTMENT_END_HOUR; i = i + (APPOINTMENT_DURATION_MINUTES + APPOINTMENT_BREAK_MINUTES)/60){
-    datetime_list.push({
+    let constData = {
       startTime: new Date(Date.UTC(year, month-1, day, Math.floor(i), (i%1)*60, 0)),
       endTime: new Date(Date.UTC(year, month-1, day, Math.floor(i), (i%1)*60+APPOINTMENT_DURATION_MINUTES, 0))
-    })
+    }
+    if (check24Hours(constData.endTime)){
+      datetime_list.push(constData);
+    }
   }
-  return(datetime_list)
+  return(datetime_list);
+}
+
+function check24Hours(date){
+  const today = new Date();
+  today.setHours(today.getHours()+24);
+  return date > today;
 }
 
 function checkInsideDates(date, start, end){
@@ -272,17 +207,70 @@ function checkDateAvailability(event_times, year, month, day){
   return availableTimes;
 }
 
+function checkIfNaN(num){
+  return Number.isNaN(num);
+}
+function checkYear(year, today){
+  if (checkIfNaN(year)){ return false; }
+  return true;
+}
+function checkMonth(month, today){
+  if (checkIfNaN(month)){ return false; }
+  if (month < 1 || month > 12 ){ return false; }
+  return true;
+}
+function checkDay(day, month, year){
+  if (checkIfNaN(day)){ return false; }
+  let constDate = new Date(Date.UTC(year, month, 0, 0, 0, 0));
+  console.log(day, constDate, constDate.getDate())
+  if (day < 0 || day > constDate.getDate() ){ return false; }
+  return true;
+}
+function checkHour(hour, today){
+  if (checkIfNaN(hour)){ return false; }
+  if (hour < 0 || hour >= 24 ){ return false }
+  return true;
+}
+function checkMinutes(minutes, today){
+  if (checkIfNaN(minutes)){ return false; }
+  if (hour < 0 || minutes >= 60 ){ return false }
+  return true;
+}
+
+function checkDateRequirements(date, today){
+  let paddedDate = today
+  paddedDate.setHours(paddedDate.getHours() + 24);
+}
+
+
+
+
+
+
+
+// - - - - - - - - - - - - - - - - - - - -
+// ROUTES
+// - - - - - - - - - - - - - - - - - - - -
 
 async function getBookableDays(req, res) {
   console.log(req.query)
-  //CHECK: valid year
-  //CHECK: valid month
-  //CHECK: GT then min month/year
-  //var minTime = new Date()
+  // Validate querys
+  const today = new Date();
+  if (
+    !checkYear(+req.query.year, today) ||
+    !checkMonth(+req.query.month, today)
+  ){
+    res.send({
+      "success": false,
+      "days": "Invalid input"
+    });
+    return
+  }
+
   var minTime = new Date(Date.UTC(+req.query.year, +(req.query.month)-1, 1, 0, 0, 0));
   var maxTime = new Date(Date.UTC(+req.query.year, +(req.query.month), 1, 0, 0, 0));
   maxTime.setDate(maxTime.getDate() - 1) //Pick first day of month then subtract 1 day
-  
+
   let auth = await getAuthCredentials();
   let events = await listEvents(auth, minTime, maxTime);
   let event_times = events.map((e) => {
@@ -315,9 +303,20 @@ async function getBookableDays(req, res) {
   //GET  /timeslots?year=yyyy&month=mm&day=dd
 */
 async function getTimeSlots(req, res) {
-  console.log(req.query);
-  //CHECK: valid year
-  //CHECK: valid month
+  console.log(req.query)
+  // Validate querys
+  const today = new Date();
+  if (
+    !checkYear(+req.query.year, today) ||
+    !checkMonth(+req.query.month, today) ||
+    !checkDay(+req.query.day, +req.query.month, +req.query.year)
+  ){
+    res.send({
+      "success": false,
+      "days": "Invalid input"
+    });
+    return
+  }
   //CHECK: valid day
   //CHECK: GT then min month/year
 
